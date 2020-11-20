49c9b60e14c3c0eb1ba86074f17245d45278e46e
Kerri Sexton <k.sexton804@gmail.com>
2020-11-19 20:40:27 -0500

    added a routes file

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,26 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+
+// ROUTING
+
+module.exports = function(app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+
+  app.get("/index", function(req, res) {
+    res.sendFile(path.join(__dirname, "../assets/index.html"));
+  });
+
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../assets/notes.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
