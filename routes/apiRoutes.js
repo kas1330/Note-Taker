5edd0697eaf55d58b9d6c717d7971a7c520f2c83
Kerri Sexton <k.sexton804@gmail.com>
2020-11-21 17:39:54 -0500

    some changes to apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,3 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
 
 
 // ===============================================================================
@@ -16,7 +11,7 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
 
   app.get("/api/notes", function(req, res) {
-    //read the json data and return it
+    //read the json data and returns it
     res.sendFile(path.join(__dirname, '/db/db.json'));
   });
 
@@ -43,13 +38,6 @@ app.delete('/api/notes', function(req,res){
 
 });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
 
 
