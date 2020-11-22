032efb47cb6b5f3646cbeddb39581fd694dba61b
Kerri Sexton <k.sexton804@gmail.com>
2020-11-21 22:34:23 -0500

    moved db.json to get correct path. now successfully addsnew notes. worked out a bunch of path related bugs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,6 @@
 
+var fs = require('fs');
+var path = require("path");
 
 // ===============================================================================
 // ROUTING
@@ -12,25 +14,28 @@ module.exports = function(app) {
 
   app.get("/api/notes", function(req, res) {
     //read the json data and returns it
-    res.sendFile(path.join(__dirname, '/db/db.json'));
+    res.sendFile(path.join(__dirname, './db.json'));
+    console.log('Path: ',__dirname, './db.json');
+
     console.log('Get works');
   });
 
   app.post('/api/notes', function(req,res){
     console.log('In the POST method');
     //parse the json file
-    var saved = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    var saved = JSON.parse(fs.readFileSync('./routes/db.json', 'utf-8'));
     console.log('POST method: ', saved);
     // req.body property contains key-value pairs of data submitted in the request body
     var newNote = req.body;
     //use the length of the object as the id
     var nId = (saved.length).toString();
+    // console.log()
     //set the new id
     newNote.id = nId;
     //push the newest note
     saved.push(newNote);
     //writes to file or creates a new file if the file doesn't already exist
-    fs.writeFileSync('./db/db.json', JSON.stringify(saved));
+    fs.writeFileSync('./routes/db.json', JSON.stringify(saved));
     console.log('Notes saved: ', newNote);
     //sends a json response, saved is sent as the body of the response
     res.json(saved);
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,9 +11,7 @@ module.exports = function(app) {
   // HTML GET Requests
   // Below code handles when users "visit" a page.
 
-//   app.get("/index", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../assets/index.html"));
-//   });
+
 
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
